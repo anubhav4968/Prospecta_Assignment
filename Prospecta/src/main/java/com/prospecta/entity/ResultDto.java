7000d8c8764abe600ab4dc397b28c2b2108afcbd
Anubhav Mishra <omanubhavmishra@gmail.com>
2023-01-09 15:05:48 +0530

    Controller Created

---
+++ Prospecta/src/main/java/com/prospecta/entity/ResultDto.java
@@ -0,0 +1,31 @@
+package com.prospecta.entity;
+
+public class ResultDto {
+
+	private String title;
+	private String description;
+	
+	public ResultDto() {
+		
+	}
+	
+	public ResultDto(String title, String description) {
+		super();
+		this.title = title;
+		this.description = description;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
